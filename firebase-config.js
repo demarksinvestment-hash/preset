3ebabdcf8ee02d3c16077fce75b9f7cfc70c631c
demarksinvestment-hash <demarksinvestment@gmail.com>
2026-03-26 13:11:30 -0500

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,10 @@
 // Rename this file values after creating your Firebase project.
 // The app loads but live sync will not work until you fill this in.
 const firebaseConfig = {
-  const firebaseConfig = {
   apiKey: "AIzaSyDNTZc36BIsBdrIJipSSaXYoBioI-n7Ydg",
   authDomain: "styl-dashboard.firebaseapp.com",
   projectId: "styl-dashboard",
   storageBucket: "styl-dashboard.firebasestorage.app",
   messagingSenderId: "460129381983",
-  appId: "1:460129381983:web:993b2498c05349996f35ea"
+  appId: "1:460129381983:web:27c087a53a002c566f35ea"
 };
